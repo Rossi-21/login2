516cf905b27c0a9ced91e23c181d8475b60048e4
Rossi21 <rossi21@gmail.com>
2023-08-03 10:02:42 -0700

    first commit

---
+++ users_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import re
+
+
+class UserManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First Name should be at least 4 characters'
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last Name should be at least 4 characters'
+        if len(postData['email']) < 1:
+            errors['email'] = 'Email is required'
+        EMAIL_REGEX = re.compile(
+            r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address!"
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
