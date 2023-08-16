153abf0f8a916d30f8e7c5e8851e98e1f0208ca9
Rossi21 <rossi21@gmail.com>
2023-08-15 20:23:16 -0700

    login 1

--- users_app/views.py
+++ users_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 from users_app.models import User
 from .models import *
@@ -14,12 +15,25 @@ def index(request):
     form2 = LoginForm()
 
     if request.method == 'POST':
+
         form1 = CreateUserForm(request.POST)
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/dashboard')
+
         if form1.is_valid():
             form1.save()
             user = form1.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + user)
             return redirect('/dashboard')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
 
     context = {
         'form1': form1,
